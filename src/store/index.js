9d2b15183b54053b97412c803ec16fccf7b4f1dc
cramk <unknown@unknown>
2021-05-10 19:52:30 -0400

    Hookup API, E2E test passing for feature

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import restaurants from './restaurants';
-
+import api from '@/api';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: {restaurants},
+  modules: {restaurants: restaurants(api)},
 });
