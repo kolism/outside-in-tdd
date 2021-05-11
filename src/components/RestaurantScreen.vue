0dcb9a5a38b79058cf4a24f0193870e001cd5458
cramk <cramk@users.noreply.github.com>
2021-05-10 21:58:25 -0400

    Revert "Creating a restaurant"

--- src/components/RestaurantScreen.vue
+++ src/components/RestaurantScreen.vue
@@ -2,17 +2,15 @@
   <v-card>
     <v-card-title>Restaurants</v-card-title>
     <v-card-text>
-      <NewRestaurantForm />
       <RestaurantList />
     </v-card-text>
   </v-card>
 </template>
 
 <script>
-import NewRestaurantForm from './NewRestaurantForm.vue';
 import RestaurantList from './RestaurantList.vue';
 export default {
-  components: {RestaurantList, NewRestaurantForm},
+  components: {RestaurantList},
   name: 'RestaurantScreen',
 };
 </script>
