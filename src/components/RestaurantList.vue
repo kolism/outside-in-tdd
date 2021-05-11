ed8c3cce1847d909280830abd51307c345075b66
cramk <unknown@unknown>
2021-05-10 21:05:49 -0400

    Add loading and error states + tests

--- src/components/RestaurantList.vue
+++ src/components/RestaurantList.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <v-progress-circular
+      v-if="loading"
+      indeterminate
+      color="primary"
+      data-testid="loading-indicator"
+    />
+
+    <v-alert type="error" data-testid="loading-error" v-if="loadError">
+      Restaurants could not be loaded.
+    </v-alert>
+
     <v-list-item
       data-testid="restaurant"
       v-for="restaurant in restaurants"
@@ -25,7 +36,11 @@ export default {
     ...mapActions({loadRestaurants: 'restaurants/load'}),
   },
   computed: {
-    ...mapState({restaurants: state => state.restaurants.records}),
+    ...mapState({
+      restaurants: state => state.restaurants.records,
+      loading: state => state.restaurants.loading,
+      loadError: state => state.restaurants.loadError,
+    }),
   },
 };
 </script>
